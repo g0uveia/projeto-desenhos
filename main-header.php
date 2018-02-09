f7be46ea0baca58052bda7c3db11db7f0f74dafc
g0uveia <36143297+g0uveia@users.noreply.github.com>
2018-02-08 21:39:43 -0300

    Add main-header.php and search.php

---
+++ main-header.php
@@ -0,0 +1,9 @@
+<header>
+	<a href="index.php"><img src="" alt="Logo"></a>
+	<form method="POST" action="search.php" style="display: inline;">
+		<input type="search" name="querry" id="search-box">
+		<button type="submit">
+			<i class="fas fa-search"></i>
+		</button>
+	</form>
+</header>
